5295ce21b5147457931b48c19e5f3d0f086d7255
Bhaumik-05 <modibhaumik180@gmail.com>
2026-07-12 11:31:18 +0530

    [FIX] Circular dependency

--- accounts/managers.py
+++ accounts/managers.py
@@ -18,4 +18,10 @@ class EmployeeManager(BaseUserManager):
         extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("role", "ADMIN")
 
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
         return self.create_user(email, password, **extra_fields)

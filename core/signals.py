f156af2dbf373a3f3347f9af19a2df7caffe87b5
Bhaumik-05 <modibhaumik180@gmail.com>
2026-07-12 12:50:51 +0530

    [FIX] core/service.py

--- core/signals.py
+++ core/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from assets.models import AssetAllocation
-from .services import create_notification
+from .service import create_notification
 
 
 @receiver(post_save, sender=AssetAllocation)

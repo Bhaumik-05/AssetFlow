3f5202094964a0a679e4d1a41740c7de4f99dbc6
Bhaumik-05 <modibhaumik180@gmail.com>
2026-07-12 12:26:41 +0530

    [Service] core app]

---
+++ core/services.py
@@ -0,0 +1,24 @@
+from .models import Notification, ActivityLog
+
+
+def create_notification(employee, title, message):
+    return Notification.objects.create(
+        employee=employee,
+        title=title,
+        message=message
+    )
+
+
+def mark_notification_as_read(notification):
+    notification.is_read = True
+    notification.save(update_fields=["is_read"])
+
+
+def log_activity(employee, module, action, object_id, ip_address):
+    return ActivityLog.objects.create(
+        employee=employee,
+        module=module,
+        action=action,
+        object_id=object_id,
+        ip_address=ip_address
+    )
---
+++ core/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from assets.models import AssetAllocation
+from .services import create_notification
+
+
+@receiver(post_save, sender=AssetAllocation)
+def notify_asset_allocation(sender, instance, created, **kwargs):
+
+    if created and instance.employee:
+
+        create_notification(
+            employee=instance.employee,
+            title="Asset Allocated",
+            message=f"{instance.asset.asset_code} has been allocated to you."
+        )
